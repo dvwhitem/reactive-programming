c94c0656e746f3c7a1860369dc295304b10d5e00
dvwhitem <dvwhitem@gmail.com>
2020-09-01 17:30:10 +0300

    add chapter-03

---
+++ chapter-03/conversion-problem/src/main/java/com/dv/conversion_problem/ConversionProblemApplication.java
@@ -0,0 +1,13 @@
+package com.dv.conversion_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionProblemApplication.class, args);
+    }
+
+}
---
+++ chapter-03/conversion-problem/src/test/java/com/dv/conversion_problem/ConversionProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dv.conversion_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
