1203794a9183f67ed1db5b4a44029b8cab624c32
dvwhitem <dvwhitem@gmail.com>
2021-01-16 13:01:30 +0200

    add flux sorted

--- chapter-04/src/test/java/com/dv/chapter04/ReactorEssentialsTest.java
+++ chapter-04/src/test/java/com/dv/chapter04/ReactorEssentialsTest.java
@@ -1,7 +1,6 @@
 package com.dv.chapter04;
 
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -11,8 +10,12 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.SortedMap;
+import java.util.stream.Collector;
 
 @Slf4j
 public class ReactorEssentialsTest {
@@ -43,7 +46,7 @@ public class ReactorEssentialsTest {
     @Test
     @Ignore
     public void endlessStreamAndCauseAnError() {
-        Flux.range(1,100)
+        Flux.range(1, 100)
                 .repeat()
                 .collectList()
                 .block();
@@ -54,7 +57,7 @@ public class ReactorEssentialsTest {
     @Ignore
     public void createFlux() {
         Flux.just("Hello", "World").doOnNext(v -> log.info("value: {}", v)).blockLast();
-        Flux.fromArray(new Integer[] {1, 4, 7}).doOnNext(v -> log.info("value: {}", v)).blockLast();
+        Flux.fromArray(new Integer[]{1, 4, 7}).doOnNext(v -> log.info("value: {}", v)).blockLast();
         Flux.fromIterable(Arrays.asList(12, 43, 54)).doOnNext(v -> log.info("value: {}", v)).blockLast();
 
         Flux<String> emptyStream = Flux.empty();
@@ -93,13 +96,64 @@ public class ReactorEssentialsTest {
     }
 
     @Test
-    public void managingSubscruption() throws InterruptedException {
+    @Ignore
+    public void managingSubscription() throws InterruptedException {
         Disposable disposable = Flux.interval(Duration.ofMillis(50))
                 .doOnCancel(() -> log.info("Cancelled")).subscribe(
                         data -> log.info("onNext: {}", data)
                 );
         Thread.sleep(300);
         disposable.dispose();
+    }
+
+    @Test
+    @Ignore
+    public void indexElements() {
+        Flux.range(2018, 5)
+                .timestamp()
+                .index()
+                .subscribe(e -> log.info("index: {}, ts: {}, value: {} data: {}",
+                        e.getT1(),
+                        Instant.ofEpochMilli(e.getT2().getT1()),
+                        e.getT2().getT2(),
+                        e.getT2().getT1()
+                        )
+                );
+
+    }
+
+    @Test
+    @Ignore
+    public void startStopStreamProcessing() throws InterruptedException {
+        Mono<?> startCommand = Mono.delay(Duration.ofSeconds(2));
+        Mono<?> stopCommand = Mono.delay(Duration.ofSeconds(5));
+
+        Flux<Long> streamOfData = Flux.interval(Duration.ofMillis(100));
+
+        streamOfData.subscribe(e -> log.info("Iterator 1  : {}", e));
+
+        streamOfData
+                .skipUntilOther(startCommand)
+                .takeUntilOther(stopCommand)
+                .subscribe(e -> log.info("Iterator 2 {} ", e));
+
+        Thread.sleep(4000);
+    }
+
+    @Test
+    @Ignore
+    public void sortedCollect() {
+        Flux.just(1, 32, 5, 54, 77, 77, 44, 343, 22, 31, 43, 12, 55)
+                .collectSortedList(Comparator.reverseOrder())
+                .subscribe(System.out::println);
+
+    }
+
+    @Test
+    public void defaultIfEmpty() {
+        Flux.just()
+                .defaultIfEmpty(1)
+                .subscribe(e -> log.info(" e: {}", e));
     }
 
     private User requestUser(String id) {
