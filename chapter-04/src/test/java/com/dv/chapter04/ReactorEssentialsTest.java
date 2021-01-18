d2e930aad70160e94e677b64ffe95366f5b5e1b2
dvwhitem <dvwhitem@gmail.com>
2021-01-18 17:16:48 +0200

    flux operators

--- chapter-04/src/test/java/com/dv/chapter04/ReactorEssentialsTest.java
+++ chapter-04/src/test/java/com/dv/chapter04/ReactorEssentialsTest.java
@@ -11,11 +11,9 @@ import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Random;
-import java.util.SortedMap;
+import java.util.*;
 import java.util.stream.Collector;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class ReactorEssentialsTest {
@@ -150,10 +148,142 @@ public class ReactorEssentialsTest {
     }
 
     @Test
+    @Ignore
     public void defaultIfEmpty() {
         Flux.just()
                 .defaultIfEmpty(1)
                 .subscribe(e -> log.info(" e: {}", e));
+    }
+
+    @Test
+    public void hasAnyElement() {
+        Flux.just(33, 3, 53, 5, 73, 87, 71, 95, 89, 1, 91)
+                .any(e -> e % 2 == 0)
+                .subscribe(hasEvens -> log.info("Has evens {}", hasEvens));
+    }
+
+    @Test
+    @Ignore
+    public void reduceElements() {
+        Flux.range(1, 5)
+                .reduce(0, (acc, elem) -> acc + elem)
+                .subscribe(result -> log.info("Result: {}", result));
+    }
+
+    @Test
+    @Ignore
+    public void scanElements() {
+        Flux.range(1, 5)
+                .scan(1, (acc, elem) -> acc + elem)
+                .subscribe(result -> log.info("Result: {}", result));
+    }
+
+    @Test
+    @Ignore
+    public void thenManyFlux() {
+        Flux.just(1, 2, 3)
+                .thenMany(Flux.just(4, 5, 6))
+                .subscribe(e -> log.info("element: {}", e));
+    }
+
+    @Test
+    @Ignore
+    public void concatFlux() {
+        Flux.concat(
+                Flux.range(1, 4),
+                Flux.just(8, 9, 10)
+        )
+                .subscribe(e -> log.info("elements: {}", e));
+    }
+
+    @Test
+    @Ignore
+    public void bufferedFlux() {
+        Flux.range(1, 17)
+                .buffer(4)
+                .subscribe(e -> log.info("elem: {}", e));
+    }
+
+    @Test
+    @Ignore
+    public void windowedFlux() {
+        Flux<Flux<Integer>> windowedFlux = Flux.range(101, 20)
+                .windowUntil(this::isPrime, true);
+
+        windowedFlux.subscribe(window -> window.collectList().subscribe(e -> log.info("window {}", e)));
+    }
+
+    @Test
+    public void groupByExample() {
+        Flux.range(1, 101)
+                .groupBy(e -> e % 2 == 0 ? "Even" : "Odd")
+                .subscribe(groupFlux -> groupFlux.scan(
+                        new LinkedList<>(),
+                        (list, elem) -> {
+                            if (list.size() > 2) {
+                                list.remove(0);
+                            }
+                            list.add(elem);
+                            return list;
+                        }
+                )
+                        .filter(arr -> !arr.isEmpty())
+                        .subscribe(data -> log.info("{}: {}", groupFlux.key(), data)));
+    }
+
+    @Test
+    @Ignore
+    public void flatMapExample() throws InterruptedException {
+        Flux
+                .just("user-1", "user-2", "user-3")
+                .flatMap(u -> requestBooks(u)
+                        .map(b -> u + "/" + b))
+                .subscribe(r -> log.info("onNext: {}", r));
+        Thread.sleep(1500);
+    }
+
+    @Test
+    @Ignore
+    public void  sampleExample() throws InterruptedException {
+        Flux.range(1, 100)
+                .delayElements(Duration.ofMillis(1))
+                .sample(Duration.ofMillis(20))
+                .subscribe(e -> log.info("e: {}", e));
+
+        Thread.sleep(1000);
+    }
+
+    @Test
+    @Ignore
+    public void doOnNextExample() {
+        Flux.just(1,2,3)
+                .concatWith(Flux.error(new RuntimeException("Conn error")))
+                .doOnEach(s -> log.info("signal: {}", s))
+                .subscribe();
+    }
+
+    @Test
+    public void signalProcessing() {
+        Flux.range(1, 3)
+                .doOnNext(e -> log.info("data: {}", e))
+                .materialize()
+                .doOnNext(e -> log.info("signal: {}", e))
+                .dematerialize()
+                .collectList()
+                .subscribe(e -> log.info("result: {}", e));
+    }
+
+    private Flux<String> requestBooks(String user) {
+        return Flux.range(1, random.nextInt(3) + 1)
+                .map(i -> "book-" + i)
+                .delayElements(Duration.ofMillis(5));
+    }
+
+    private boolean isPrime(int number) {
+        return number > 2
+                && IntStream
+                .rangeClosed(2, (int) Math.sqrt(number))
+                .noneMatch(n -> (number % n) == 0);
     }
 
     private User requestUser(String id) {
