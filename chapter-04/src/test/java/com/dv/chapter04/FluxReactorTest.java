c59a5c32dc50662e1c00018f676a85003c59725c
dvwhitem <dvwhitem@gmail.com>
2021-11-11 17:46:06 +0200

    polish

--- chapter-04/src/test/java/com/dv/chapter04/FluxReactorTest.java
+++ chapter-04/src/test/java/com/dv/chapter04/FluxReactorTest.java
@@ -3,6 +3,8 @@ package com.dv.chapter04;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class FluxReactorTest {
@@ -20,4 +22,60 @@ public class FluxReactorTest {
                 .collectList()
                 .block();
     }
+
+    @Test
+    public void fluxRange() {
+        var fluxRange = Flux.range(2010, 12)
+                .take(3)
+                .repeat(3)
+                .collectList()
+                .block();
+
+        log.info("{}", fluxRange);
+    }
+
+    @Test
+    public void monoCallable() {
+        Mono<String> mono = Mono.fromCallable(this::httpRequest).log();
+        StepVerifier.create(mono)
+                .expectSubscription()
+                .expectErrorMessage("IO error")
+                .verify();
+    }
+
+    @Test
+    public void shouldCreateDefer() {
+        Mono<User> userMono = requestUserData(null).log();
+        StepVerifier.create(userMono)
+                .expectNextCount(0)
+                .expectErrorMessage("Invalid user id")
+                .verify();
+    }
+
+
+    public Mono<User> requestUserData(String userId) {
+        return Mono.defer(() -> isValid(userId)
+                ? Mono.fromCallable(() -> requestUser(userId))
+                : Mono.error(new IllegalArgumentException("Invalid user id"))
+                );
+    }
+
+    private String httpRequest() {
+        log.info("Making HTTP request");
+        throw new RuntimeException("IO error");
+    }
+
+    private boolean isValid(String userId) {
+        return userId != null;
+    }
+
+    private User requestUser(String id) {
+        return new User();
+    }
+
+    record User(String id, String name) {
+        public User() {
+            this(null, null);
+        }
+    };
 }
